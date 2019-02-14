f2c9090331e8d1a897ed54c177d69fc0a063d7f8
taz <t.chisti@gmail.com>
2019-02-14 08:30:13 -0500

    making chart interactive

--- NYC_Housing/static/js/Chart.js
+++ NYC_Housing/static/js/Chart.js
@@ -1,29 +1,46 @@
-var trace = {
-    
-    "x": [
-    "BROOKLYN",
-    "MANHATTAN",
-    "QUEENS",
-    "BRONX",
-    "STATEN ISLAND",
-        ],
-    "y": [
-    42814,
-    34078,
-    28320,
-    27779,
-    4607,
-    ],
-    "type": "bar",
-    color: "purple",
-        opacity: 0.5
-    }
+function buildCharts(firstOffense){
 
-    var data = [trace]
-
-    var layout = {
-        title: "Felonies per Borough",
+  // var url =`/crime/${firstOffense}`
+  d3.json(`/crime/${firstOffense}`).then((trace)=>{
+    console.log(trace);
+  
+      var data = [trace];
+  
+      var layout = {
+          title: "Felonies per Borough",
+          
+      
+        };
+      Plotly.newPlot("plot",data,layout);
+  
+      });
+  }
+  
+  
+      function init() {
+          // Grab a reference to the dropdown select element
+          var selector = d3.select("#offense");
         
+          // Use the list of sample names to populate the select options
+          d3.json("/offense").then((sampleNames) => {
+            sampleNames.forEach((sample) => {
+              selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+            });
         
-      };
-    Plotly.newPlot("plot",data,layout);
+            // Use the first sample from the list to build the initial plots
+            const firstOffense = sampleNames[0];
+            console.log(firstOffense)
+            buildCharts(firstOffense);
+            
+          });
+        }
+  
+        function optionChanged(firstOffense) {
+          // Fetch new data each time a new sample is selected
+          buildCharts(firstOffense);
+        }
+  
+        init();
